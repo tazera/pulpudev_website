d7d3daf3388954481da5cb598a7cbd8f4dbc25c3
Lyubomir <43777483+tazera@users.noreply.github.com>
2025-05-20 12:07:42 +0300

    deploy change

--- dockerize.sh
+++ dockerize.sh
@@ -1,4 +1,9 @@
 docker rm -f pulpudev
 docker rmi pulpudev
 docker build -t pulpudev .
-docker run -d -p 8080:8080 --name pulpudev pulpudev
+docker run -d
+-e VIRTUAL_HOST=pulpudev.com
+-e LETSENCRYPT_HOST=pulpudev.com
+--network net
+--name pulpudev
+pulpudev
