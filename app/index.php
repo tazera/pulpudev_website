4a0c208633322d8a4b6160d9769d8aeda9d82fec
Lyubomir <43777483+tazera@users.noreply.github.com>
2025-07-08 13:46:29 +0300

    test

--- app/index.php
+++ app/index.php
@@ -1,4 +1,12 @@
 <?php
-// header("Location: /pages/home/home.php");
-header("Location: /");
-exit;
+// Get the current URL path
+$current_url = $_SERVER['REQUEST_URI'];
+
+// If not at the root URL, redirect to root
+if ($current_url != '/' && $current_url != '/index.php') {
+    header("Location: /");
+    exit;
+}
+
+// Otherwise include the home page content
+include_once($_SERVER['DOCUMENT_ROOT'] . '/pages/home/home.php');
